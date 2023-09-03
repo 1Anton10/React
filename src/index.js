4f6f460c82ae04a90a2283e2e8e5c937580d69ad
retr0 <45705896+1Anton10@users.noreply.github.com>
2023-09-03 16:10:20 +0500

    New folders

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './reset_style.css';
-import './index.css';
+import './css/reset_style.css';
+import './css/index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
